456d27854291cd39766aaf8e84ab413dcb932328
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2023-05-16 15:49:38 -0600

    Delete iotc-python-sdk-fork1-main directory
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- iotc-python-sdk-fork1-main/reinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Uninstalling old SDK"
-echo "---------------------------------"
-pip3 uninstall iotconnect_sdk -y
-echo ""
-echo "Installing new SDK"
-echo "---------------------------------"
-pip3 install --user .
