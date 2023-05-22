7983af19f004ac03f6edeb5bd9b7533c544321ef
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2023-05-22 02:54:38 -0600

    Update STM32MP157F-DK2_Basic_Demo.py
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- STM32MP157F-DK2_Demo/iotconnect-python-sdk-v1.0/sample/STM32MP157F-DK2_Basic_Demo.py
+++ STM32MP157F-DK2_Demo/iotconnect-python-sdk-v1.0/sample/STM32MP157F-DK2_Basic_Demo.py
@@ -34,8 +34,8 @@ device_list=[]
 
 SdkOptions={
 	"certificate" : { 
-		"SSLKeyPath"  : "/home/weston/Demo/STM32MP157-certificates/pk_STM32MP157.pem", 
-		"SSLCertPath" : "/home/weston/Demo/STM32MP157-certificates/cert_STM32MP157.crt",
+		"SSLKeyPath"  : "/home/weston/Demo/STM32MP157F-certificates/pk_STM32MP157F.pem", 
+		"SSLCertPath" : "/home/weston/Demo/STM32MP157F-certificates/cert_STM32MP157F.crt",
 		"SSLCaPath"   : "/home/weston/Demo/iotc-pov-engineering-main/STM32MP157F-DK2_Demo/iotconnect-python-sdk-v1.0/sample/aws_cert/root-CA.pem"
 	},
     "offlineStorage":{
