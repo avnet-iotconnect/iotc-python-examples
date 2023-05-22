e18cc16e1fac6b55a776979a2c19c9338b1a5f2a
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2023-05-22 02:52:29 -0600

    Update STM32MP157_setup.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- STM32MP157F-DK2_Demo/STM32MP157_setup.sh
+++ STM32MP157F-DK2_Demo/STM32MP157_setup.sh
@@ -3,10 +3,10 @@
 cd iotconnect-python-sdk-v1.0
 cd iotconnect-sdk-1.0
 pip3 install --user .
-sudo mkdir /media/usbdrive
+mkdir /media/usbdrive
 echo -e "Please insert your flash drive with your certificates folder into a USB port on this device, and then hit ENTER. "
 read input
-sudo mount /dev/sda1 /media/usbdrive || sudo mount /dev/sdb1 /media/usbdrive
+mount /dev/sda1 /media/usbdrive || sudo mount /dev/sdb1 /media/usbdrive
 
 cp -r /media/usbdrive/STM32MP157-certificates /home/weston/Demo
 
