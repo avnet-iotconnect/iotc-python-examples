122d64049867c607719c06142d01455ba78ed458
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2023-05-26 14:39:28 -0600

    Update STM32MP157_setup.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- STM32MP157F-DK2_Demo/STM32MP157_setup.sh
+++ STM32MP157F-DK2_Demo/STM32MP157_setup.sh
@@ -6,7 +6,7 @@ pip3 install --user .
 mkdir /media/usbdrive
 echo -e "Please insert your flash drive with your certificates folder into a USB port on this device, wait 5 seconds, and then hit ENTER. "
 read input
-mount /dev/sda1 /media/usbdrive || mount /dev/sdb1 /media/usbdrive
+mount /dev/sda1 /media/usbdrive || mount /dev/sdb1 /media/usbdrive || mount /dev/sdc1 /media/usbdrive || mount /dev/sdd1 /media/usbdrive
 
 cp -r /media/usbdrive/STM32MP157F-certificates /home/weston/Demo
 
