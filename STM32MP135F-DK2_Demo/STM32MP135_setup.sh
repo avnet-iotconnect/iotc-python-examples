6ffb25ea1ff8a72cb0c8b590d7ba91b54c591b41
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2023-05-31 13:36:52 -0600

    Update and rename STM32MP157_setup.sh to STM32MP135_setup.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- STM32MP135F-DK2_Demo/STM32MP135_setup.sh
+++ STM32MP135F-DK2_Demo/STM32MP135_setup.sh
@@ -3,10 +3,10 @@
 cd iotconnect-python-sdk-v1.0
 cd iotconnect-sdk-1.0
 pip3 install --user .
-cd /home/weston/Demo/iotc-pov-engineering-main/STM32MP157F-DK2_Demo/iotconnect-python-sdk-v1.0/sample
+cd /home/weston/Demo/iotc-pov-engineering-main/STM32MP135F-DK2_Demo/iotconnect-python-sdk-v1.0/sample
 mkdir /media/usbdrive
 echo -e "Please insert your flash drive with your certificates folder into a USB port on this device, wait 5 seconds, and then hit ENTER. "
 read input
 mount /dev/sda1 /media/usbdrive || mount /dev/sdb1 /media/usbdrive || mount /dev/sdc1 /media/usbdrive || mount /dev/sdd1 /media/usbdrive
 
-cp -r /media/usbdrive/STM32MP157F-certificates /home/weston/Demo
+cp -r /media/usbdrive/STM32MP135F-certificates /home/weston/Demo
