3855dc1e2dc8d19456927c3553fe42606025fee2
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2023-06-05 14:54:45 -0600

    Update PE100_Basic_AWS_Demo.py
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- PE100A_Basic_AWS_Demo/PE100_Basic_AWS_Demo.py
+++ PE100A_Basic_AWS_Demo/PE100_Basic_AWS_Demo.py
@@ -8,14 +8,14 @@ from datetime import datetime
 import os
 import getopt
 
+# Get CPID and Environment values from command-line options
 cpid = ""
 env = ""
-
 argv = sys.argv[1:]
-
 try:
     opts, args = getopt.getopt(argv,"c:e:")
-except:
+except Exception as ex:
+    print(ex)
     print("Options error")
 for opt, arg in opts:
     if opt == '-c':
@@ -23,47 +23,20 @@ for opt, arg in opts:
     elif opt == '-e':
         env = arg
 
-"""
-* ## Prerequisite parameter to run this sampel code
-* uniqueId     :: Its device ID which register on IotConnect platform and also its status has Active and Acquired
-* cpId         :: It need to get from the IoTConnect platform "Settings->Key Vault". 
-* env          :: It need to get from the IoTConnect platform "Settings->Key Vault". 
-* SId 	       :: SId is the company code. You can get it from the IoTConnect UI portal "Settings -> Key Vault -> SDK Identities -> select language Python and Version 1.0"
-* interval     :: send data frequency in seconds
-* sdkOptions   :: It helps to define the path of self signed and CA signed certificate as well as define the offlinne storage configuration.
-"""
-
+	
 UniqueId = "PE100Demo"
 SId = ""
-
 Sdk=None
 interval = 5
 directmethodlist={}
 ACKdirect=[]
 device_list=[]
-"""
-* sdkOptions is optional. Mandatory for "certificate" X.509 device authentication type
-* "certificate" : It indicated to define the path of the certificate file. Mandatory for X.509/SSL device CA signed or self-signed authentication type only.
-* 	- SSLKeyPath: your device key
-* 	- SSLCertPath: your device certificate
-* 	- SSLCaPath : Root CA certificate
-* 	- Windows + Linux OS: Use "/" forward slash (Example: Windows: "E:/folder1/folder2/certificate", Linux: "/home/folder1/folder2/certificate")
-* "offlineStorage" : Define the configuration related to the offline data storage 
-* 	- disabled : false = offline data storing, true = not storing offline data 
-* 	- availSpaceInMb : Define the file size of offline data which should be in (MB)
-* 	- fileCount : Number of files need to create for offline data
-* "devicePrimaryKey" : It is optional parameter. Mandatory for the Symmetric Key Authentication support only. It gets from the IoTConnect UI portal "Device -> Select device -> info(Tab) -> Connection Info -> Device Connection".
-    - - "devicePrimaryKey": "<<your Key>>"
-* Note: sdkOptions is optional but mandatory for SSL/x509 device authentication type only. Define proper setting or leave it NULL. If you not provide the offline storage it will set the default settings as per defined above. It may harm your device by storing the large data. Once memory get full may chance to stop the execution.
-"""
-
 
 SdkOptions={
 	"certificate" : { 
 		"SSLKeyPath"  : "/home/asus/PE100_Demo/PE100_Demo-certificates/pk_PE100_Demo.pem", 
 		"SSLCertPath" : "/home/asus/PE100_Demo/PE100_Demo-certificates/cert_PE100_Demo.crt",
 		"SSLCaPath"   : "/home/asus/PE100_Demo/root-CA.pem"
-        
 	},
     "offlineStorage":{
         "disabled": False,
@@ -74,16 +47,7 @@ SdkOptions={
     "skipValidation":False,
     "discoveryUrl":"https://awsdiscovery.iotconnect.io/",
     "IsDebug": False
-   
 }
-
-
-"""
- * Type    : Callback Function "DeviceCallback()"
- * Usage   : Firmware will receive commands from cloud. You can manage your business logic as per received command.
- * Input   :  
- * Output  : Receive device command, firmware command and other device initialize error response 
-"""
 
 def DeviceCallback(msg):
     global Sdk
@@ -94,16 +58,6 @@ def DeviceCallback(msg):
         cmdType = msg["ct"] if "ct"in msg else None
     # Other Command
     if cmdType == 0:
-        """
-        * Type    : Public Method "sendAck()"
-        * Usage   : Send device command received acknowledgment to cloud
-        * 
-        * - status Type
-        *     st = 6; // Device command Ack status 
-        *     st = 4; // Failed Ack
-        * - Message Type
-        *     msgType = 5; // for "0x01" device command 
-        """
         data=msg
         if data != None:
             if "id" in data:
@@ -125,15 +79,6 @@ def DeviceFirmwareCallback(msg):
         cmdType = msg["ct"] if msg["ct"] != None else None
 
     if cmdType == 1:
-        """
-        * Type    : Public Method "sendAck()"
-        * Usage   : Send firmware command received acknowledgement to cloud
-        * - status Type
-        *     st = 7; // firmware OTA command Ack status 
-        *     st = 4; // Failed Ack
-        * - Message Type
-        *     msgType = 11; // for "0x02" Firmware command
-        """
         data = msg
         if data != None:
             if ("urls" in data) and data["urls"]:
@@ -153,20 +98,7 @@ def DeviceConectionCallback(msg):
     if cmdType == 116:
         #Device connection status e.g. data["command"] = true(connected) or false(disconnected)
         print(json.dumps(msg))
-
-"""
- * Type    : Public Method "UpdateTwin()"
- * Usage   : Update the twin reported property
- * Input   : Desired property "key" and Desired property "value"
- * Output  : 
-"""
-
-"""
- * Type    : Callback Function "TwinUpdateCallback()"
- * Usage   : Manage twin properties as per business logic to update the twin reported property
- * Input   : 
- * Output  : Receive twin Desired and twin Reported properties
-"""
+	
 def TwinUpdateCallback(msg):
     global Sdk
     if msg:
@@ -177,12 +109,6 @@ def TwinUpdateCallback(msg):
                 if ("version" not in j) and ("uniqueId" not in j):
                     Sdk.UpdateTwin(j,msg["desired"][j])
 
-"""
- * Type    : Public data Method "SendData()"
- * Usage   : To publish the data on cloud D2C 
- * Input   : Predefined data object 
- * Output  : 
-"""
 def sendBackToSDK(sdk, dataArray):
     sdk.SendData(dataArray)
     time.sleep(interval)
@@ -216,38 +142,11 @@ def attributeDetails(data):
     print ("attribute received in firmware")
     print (data)
     
-
-
-
 def main():
     global SId,cpid,env,SdkOptions,Sdk,ACKdirect,device_list
-    
     try:
-        """
-        if SdkOptions["certificate"]:
-            for prop in SdkOptions["certificate"]:
-                if os.path.isfile(SdkOptions["certificate"][prop]):
-                    pass
-                else:
-                    print("please give proper path")
-                    break
-        else:
-            print("you are not use auth type CA sign or self CA sign ") 
-        """    
-        """
-        * Type    : Object Initialization "IoTConnectSDK()"
-        * Usage   : To Initialize SDK and Device cinnection
-        * Input   : cpId, uniqueId, sdkOptions, env as explained above and DeviceCallback and TwinUpdateCallback is callback functions
-        * Output  : Callback methods for device command and twin properties
-        """
         with IoTConnectSDK(UniqueId,SId,cpid,env,SdkOptions,DeviceConectionCallback) as Sdk:
             try:
-                """
-                * Type    : Public Method "GetAllTwins()"
-                * Usage   : Send request to get all the twin properties Desired and Reported
-                * Input   : 
-                * Output  : 
-                """
                 Sdk.onDeviceCommand(DeviceCallback)
                 Sdk.onTwinChangeCommand(TwinUpdateCallback)
                 Sdk.onOTACommand(DeviceFirmwareCallback)
@@ -266,8 +165,7 @@ def main():
             except KeyboardInterrupt:
                 print ("Keyboard Interrupt Exception")
                 os.abort()
-                
-                
+                 
     except Exception as ex:
         print(ex)
         sys.exit(0)
