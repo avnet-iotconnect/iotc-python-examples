3b9d2210a23bbc1c0014f58042a2f7f83ab4cb1d
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2023-06-05 13:19:52 -0600

    Update PE100_setup.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- PE100A_Basic_AWS_Demo/PE100_setup.sh
+++ PE100A_Basic_AWS_Demo/PE100_setup.sh
@@ -2,17 +2,7 @@
 
 sudo apt-get update
 
-echo "" >> /etc/network/interfaces
-echo "auto enp1s0" >> /etc/network/interfaces
-echo "iface enp1s0 inet dhcp" >> /etc/network/interfaces
-echo "	dns-nameservers 8.8.8.8" >> /etc/network/interfaces
-echo "" >> /etc/network/interfaces
-echo "auto eth0" >> /etc/network/interfaces
-echo "iface eth0 inet static" >> /etc/network/interfaces
-echo "        address 169.254.169.2" >> /etc/network/interfaces
-echo "        gateway 169.254.169.1" >> /etc/network/interfaces
-
-sudo apt install unzip nano python3-pip -y
+sudo apt install unzip python3-pip -y
 cd /home/asus/PE100_Demo
 wget https://help.iotconnect.io/wp-content/uploads/2023/01/iotconnect-python-sdk-v1.0.zip
 unzip iotconnect-python-sdk-v1.0.zip
@@ -25,13 +15,10 @@ echo -e "Please insert your flash drive with your certificates folder into a USB
 read input
 sudo mount /dev/sda1 /media/usbdrive || sudo mount /dev/sdb1 /media/usbdrive
 
-cd /media/usbdrive
-cp ./pk_PE100_Demo.pem ./cert_PE100_Demo.crt /home/asus/PE100_Demo
+cp -r /media/usbdrive/PE100_Demo-certificates /home/asus/PE100_Demo
 
 cd /home/asus/PE100_Demo/iotc-pov-engineering/PE100A_Motor_Monitor_Demo/
-cp ./ADAM.py ./ModbusDevice.py ./PE100_Motor_Monitor_Demo.py /home/asus/PE100_Demo/iotconnect-python-sdk-v1.0/sample
+cp ./PE100_Basic_AWS_Demo.py /home/asus/PE100_Demo/iotconnect-python-sdk-v1.0/sample
 cp ./root-CA.pem /home/asus/PE100_Demo
-
-reboot
 
 
