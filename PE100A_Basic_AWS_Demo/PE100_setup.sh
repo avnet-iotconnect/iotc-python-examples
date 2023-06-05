63fd3ae9be1ec9f2ab8ac211a6af0c3df7157d40
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2023-06-05 13:36:11 -0600

    Update PE100_setup.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- PE100A_Basic_AWS_Demo/PE100_setup.sh
+++ PE100A_Basic_AWS_Demo/PE100_setup.sh
@@ -8,7 +8,6 @@ wget https://help.iotconnect.io/wp-content/uploads/2023/01/iotconnect-python-sdk
 unzip iotconnect-python-sdk-v1.0.zip
 cd iotconnect-python-sdk-v1.0
 pip3 install iotconnect-sdk-1.0.tar.gz
-pip3 install -U minimalmodbus pymodbus
 
 sudo mkdir /media/usbdrive
 echo -e "Please insert your flash drive with your certificates folder into a USB port on this device, and then hit ENTER. "
@@ -17,7 +16,7 @@ sudo mount /dev/sda1 /media/usbdrive || sudo mount /dev/sdb1 /media/usbdrive
 
 cp -r /media/usbdrive/PE100_Demo-certificates /home/asus/PE100_Demo
 
-cd /home/asus/PE100_Demo/iotc-pov-engineering/PE100A_Motor_Monitor_Demo/
+cd /home/asus/PE100_Demo/iotc-pov-engineering/PE100A_Basic_AWS_Demo/
 cp ./PE100_Basic_AWS_Demo.py /home/asus/PE100_Demo/iotconnect-python-sdk-v1.0/sample
 cp ./root-CA.pem /home/asus/PE100_Demo
 
