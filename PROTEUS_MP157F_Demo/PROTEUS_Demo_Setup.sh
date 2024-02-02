b0f32a929066889bd6be176cb440bfe7af49ece9
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2024-02-02 10:36:11 -0700

    Update PROTEUS_Demo_Setup.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- PROTEUS_MP157F_Demo/PROTEUS_Demo_Setup.sh
+++ PROTEUS_MP157F_Demo/PROTEUS_Demo_Setup.sh
@@ -9,8 +9,6 @@ chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 pip3 install bleak
-mkdir /home/weston/certs
-cp -r /media/usbdrive/STM32MP157F-certificates /home/weston/certs
 cp -r /media/usbdrive/PROTEUS_MP157F_Demo /home/weston
 cd /home/weston/PROTEUS_MP157F_Demo/iotconnect-python-sdk-v1.0/iotconnect-sdk-v1.0
 python3 setup.py install
