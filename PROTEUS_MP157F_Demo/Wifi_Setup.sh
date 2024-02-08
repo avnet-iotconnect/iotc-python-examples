a09457a4d8e2e941bc250278bbd4e3f40266e3ff
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2024-02-08 13:41:01 -0700

    Update Wifi_Setup.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- PROTEUS_MP157F_Demo/Wifi_Setup.sh
+++ PROTEUS_MP157F_Demo/Wifi_Setup.sh
@@ -10,19 +10,23 @@ while true; do
     break # Valid input given so exit the loop.
 done
 
-if [ "$response" == "N" ]; then
-    ifconfig wlan0 up
-    echo "[Match]" > /lib/systemd/network/51-wireless.network
-    echo "Name=wlan0" >> /lib/systemd/network/51-wireless.network
-    echo "[Network]" >> /lib/systemd/network/51-wireless.network
-    echo "DHCP=ipv4" >> /lib/systemd/network/51-wireless.network
-    mkdir -p /etc/wpa_supplicant/
-    echo "ctrl_interface=/var/run/wpa_supplicant" > /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-    echo "eapol_version=1" >> /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-    echo "ap_scan=1" >> /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-    echo "fast_reauth=1" >> /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-    echo "" >> /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+if [ "$response" == "Y" ]; then
+    systemctl stop wpa_supplicant@wlan0.service
+    systemctl disable wpa_supplicant@wlan0.service
+    rm -r /etc/wpa_supplicant
 fi
+
+ifconfig wlan0 up
+echo "[Match]" > /lib/systemd/network/51-wireless.network
+echo "Name=wlan0" >> /lib/systemd/network/51-wireless.network
+echo "[Network]" >> /lib/systemd/network/51-wireless.network
+echo "DHCP=ipv4" >> /lib/systemd/network/51-wireless.network
+mkdir -p /etc/wpa_supplicant/
+echo "ctrl_interface=/var/run/wpa_supplicant" > /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+echo "eapol_version=1" >> /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+echo "ap_scan=1" >> /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+echo "fast_reauth=1" >> /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+echo "" >> /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 
 while true; do
     echo "Please type your wireless network name, also known as the SSID, and press ENTER"
@@ -45,8 +49,6 @@ while true; do
 done
 
 wpa_passphrase $ssid $password >> /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-if [ "$response" == "N" ]; then
-    systemctl enable wpa_supplicant@wlan0.service
-fi
+systemctl enable wpa_supplicant@wlan0.service
 systemctl restart systemd-networkd.service
 systemctl restart wpa_supplicant@wlan0.service
