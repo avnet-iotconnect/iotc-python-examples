c9a04395ff292eac0514ff6931294edd8e7c91f5
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2024-03-20 15:32:01 -0600

    Update SAMA5D27_Demo_Setup.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- SAMA5D27_Basic_Demo/SAMA5D27_Demo_Setup.sh
+++ SAMA5D27_Basic_Demo/SAMA5D27_Demo_Setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/root/SAMA5D27_Basic_Demo/iotconnect-python-sdk-v1.0/iotconnect-sdk-1.0
+cd /home/root/SAMA5D27_Basic_Demo/iotconnect-sdk/
 python3 setup.py install
 pip3 install paho-mqtt==1.6.1
 cp /home/root/SAMA5D27_Basic_Demo/iotconnect.service /etc/systemd/system
