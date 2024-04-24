f846f86fe40df684854278353c2bf8cf94f687ab
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2024-04-24 16:43:57 -0600

    Update PROTEUS_Demo_Setup.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- PROTEUS_MP157F_Demo/PROTEUS_Demo_Setup.sh
+++ PROTEUS_MP157F_Demo/PROTEUS_Demo_Setup.sh
@@ -14,3 +14,5 @@ python3 setup.py install
 pip3 install paho-mqtt==1.6.1
 cp /home/weston/PROTEUS_MP157F_Demo/iotconnect.service /etc/systemd/system
 systemctl enable iotconnect
+cd /home/weston/PROTEUS_MP157F_Demo
+pip3 install blue_st_sdk-1.5.0-py3-none-any.whl
