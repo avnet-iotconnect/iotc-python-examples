9008b9309fea58b9dc98f1e8c0ac77259ac64577
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2024-04-27 13:27:02 -0600

    Update MP157F_Demo_Setup.sh
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- STM32MP157F-DK2_Demo/MP157F_Demo_Setup.sh
+++ STM32MP157F-DK2_Demo/MP157F_Demo_Setup.sh
@@ -8,9 +8,11 @@ chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 pip3 install bleak
-cp -r /media/usbdrive/PROTEUS_MP157F_Demo /home/weston
-cd /home/weston/PROTEUS_MP157F_Demo/iotconnect-python-sdk-v1.0/iotconnect-sdk-1.0
+cp -r /media/usbdrive/STM32MP157F-DK2_Demo /home/weston
+cd /home/weston/STM32MP157F-DK2_Demo/iotconnect-python-sdk-v1.0/iotconnect-sdk-1.0
 python3 setup.py install
 pip3 install paho-mqtt==1.6.1
-cp /home/weston/PROTEUS_MP157F_Demo/iotconnect.service /etc/systemd/system
+cd /home/weston/STM32MP157F-DK2_Demo
+pip3 install blue_st_sdk-1.5.0-py3-none-any.whl
+cp /home/weston/STM32MP157F-DK2_Demo/iotconnect.service /etc/systemd/system
 systemctl enable iotconnect
