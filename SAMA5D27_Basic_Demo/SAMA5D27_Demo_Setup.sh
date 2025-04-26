abf5c474bd7b2bd46bbbc33a6cd1109ef32af78c
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-04-25 18:13:41 -0600

    Delete SAMA5D27_Basic_Demo directory
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- SAMA5D27_Basic_Demo/SAMA5D27_Demo_Setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/root/SAMA5D27_Basic_Demo/iotconnect-sdk/
-python3 setup.py install
-pip3 install paho-mqtt==1.6.1
-cp /home/root/SAMA5D27_Basic_Demo/iotconnect.service /etc/systemd/system
-systemctl enable iotconnect
-reboot
--- SAMA5D27_Basic_Demo/iotconnect-sdk/iotconnect/__init__.py
+++
@@ -1,1 +0,0 @@
-from .IoTConnectSDK import IoTConnectSDK
